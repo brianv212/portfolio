53db8988e4f0ac5813679c76e7a3d6e23b4d7319
brianv212 <49389758+brianv212@users.noreply.github.com>
2021-06-29 02:27:37 -0700

    Add files via upload

--- projects.js
+++ projects.js
@@ -6,9 +6,7 @@ var titles = [[
     "CS 178: Machine Learning and Data Mining",
     "CS 171: Intro to Artificial Intelligence",
     "CS 121: Information Retrieval",
-    "CS 132: Computer Networks",
-    "CS 163: Graph Algorithms",
-    "CS 122A: ntroduction to Data Management",
+    "CS 122A: Introduction to Data Management",
     "Informatics 131: Human Computer Interaction",
     "CS 117: Computer Vision"
 ],[
@@ -52,48 +50,46 @@ var text = [[
     "Probably one of the harder courses I've taken. Considering I've never never\
     really dealt with Machine Learning, there was definitely a lot to take in over the\
     Fall quarter (2020). Upon learning about many different ML techniques, the final\
-    assignment for this class was to create a learner to compete in a Kaggle Competition.\
-    </br></br>For me personally, I'm interested in the idea of machine learning. It's definitely\
-    a wonder how one can make a machine learn to accurately predict outcomes based purely on\
-    past events, but delving deeper into the topic made me feel like this was a whole new\
-    realm of coding that would be quite difficult to wrap my head around.\
-    </br></br>Nonetheless, I did my fair share of learning and produced a learner that would predict\
-    close to 75% in this kaggle competition. Definitely not the best but a great feat for myself.",
+    assignment for this class was to create a learner to compete in a Kaggle Competition.",
 
     "It's always interesting seeing how bots would perform against an actual player\
     in video games. How do people manage to get an AI to outperform humans? My intro\
     to Artificial Intelligence class helped me understand the basics of MCTS and a/b\
     pruning, which led to the creation of my own little AI that'd be able to go head to\
-    head with above average players.\
-    </br></br>The game in question was no other than checkers, and my partner and I had quite\
-    a big hurdle to jump over before having a competent AI. In our early stages of development,\
-    we hadn't quite grasped the idea of MCTS, so we would play against other AI's with no clear\
-    indication of a straight-thinking AI - randomly taking moves wasn't enough to clearly gauge\
-    a functioning AI.\
-    </br></br>It wasn't until midway where we realized that we needed to make a tree (who could've guessed that)\
-    rather than trying to create our own methods of a monte carlo tree searcher. By the end of it all,\
-    our AI was doing quite well. Passable when going against AIs of increasing intelligence.\
-    </br></br>It is a very interesting topic for me, and something I'd like to touch up on again in the future.",
+    head with above average players.",
 
     "The Web contains so much that it'd make one wonder how they'd manage to store\
     information on the websites. Better yet, it makes people wonder how it is that\
     when we search for something, a certain selection of links are chosen in a certain\
     order. My information retrieval course taught me the basics on web search engines and how\
     they work - how they rank websites and how they go about retrieving that information. In\
-    the end, I was able to create a mini search engine for UCI-related links."
+    the end, I was able to create a mini search engine for UCI-related links.",
+
+    "There's a ton of information out there and it can get hard to manage once you start\
+    getting more and more. This class introduced the basics of data management, having us\
+    use a common program to guide along the way (MySQL).",
+
+    "I am pretty interested in the front end of things, as well as how applications are\
+    designed in such a manner where it would be most appealing to the user. In this class,\
+    I learned all about it, and it was a nice little introduction to UI/UX.",
+
+    "This was labeled as a project course, and understandably so as I went in depth to learn\
+    about the intricacies of translating an image to 3D spaces. The class introduced a variety\
+    of topics, starting with the very basics to get an understanding of how this idea works.\
+    And in the end of it all, I successfully created a model purely from images - one of my greatest\
+    accomplishments."
 ]]
 
-// a href=\"https://github.com/brianv212/algorithm_racing\" target=\"_blank\">Github Repository</a>"
-
 var text_full = [[
-    "</br></br>(<a onclick=\"redirect('https://www.youtube.com/watch?v=kPRA0W1kECg')\" class='link'>The YouTube video in question</a>) Volume warning!\
+    "</br></br>(<a href=\"https://www.youtube.com/watch?v=kPRA0W1kECg\" target=\"_blank\">The video in question</a>) Volume warning!\
     </br></br>This was my first ever website created, and my first time ever using React. Looking back now, this definitely\
     could've been done in a much shorter period of time, but what mattered to me was that I took something out of\
     if, and that was a better understanding of React.\
     </br></br>As for the toughest challenge here, it was understanding how to 'animate' the bars moving on the screen. After many days\
     later and many hours spent learning how to style it, how to use Javascript Promises and async, and how to properly implement those\
     ideas into the algorithms, I was able to create this neat little website.\
-    </br></br><a href=\"https://github.com/brianv212/algorithm_racing\" target=\"_blank\">Github Repository</a>",
+    </br></br><a href=\"https://github.com/brianv212/algorithm_racing\" target=\"_blank\">Github Repository</a>\
+    </br><a href=\"https://brianv212.github.io/algorithm_racing/\" target=\"_blank\">Website</a>",
 
     "</br></br>This was done in January after I had just found out about Rocket League.\
     I am by no means any good at the game, but as with any other game, it's always refreshing\
@@ -105,7 +101,7 @@ var text_full = [[
     time and time again. This personal project really tested my\
     ability to use APIs and enhanced my ability to read and gather data. In the end, I created a simple\
     web application that gathers information on upcoming eSporting events, Rocket League included!\
-    </br></br><a onclick=\"redirect('https://github.com/brianv212/eSportsTracker')\" class='link'>Github Repository</a>",
+    </br></br><a href=\"https://github.com/brianv212/eSportsTracker\" target=\"_blank\">Github Repository</a>",
 
     "</br></br>I've always wondered about this, and it wasn't until a friend came\
     up to me saying how she wanted to ask me something, but didn't really want me to\
@@ -117,7 +113,7 @@ var text_full = [[
     of Firebase, but with some determination it came by easily.\
     It's definitely not a perfect application, but given more time I would like to\
     add more features to it and make it a cleaner-looking application!\
-    </br></br><a onclick=\"redirect('https://github.com/brianv212/questions_from_a_stranger')\" class='link'>Github Repository</a>",
+    </br></br><a href=\"https://github.com/brianv212/questions_from_a_stranger\" target=\"_blank\">Github Repository</a>",
     "</br></br>We weren't going for any of the prizes. We simply wanted to teach a good friend how to use the language.\
     It was also a plus being able to refine the skills I already have with this project. When thinking about ideas, we noticed\
     a particular category about spreading awareness about Covid-19 using any technology. We went with this idea because it was\
@@ -126,43 +122,58 @@ var text_full = [[
     using very simple methods. The idea was having infected and noninfected characters in a confined area, and showing how quickly\
     a disease could spread with a lack of a mask.\
     </br></br>It was a very fun process and we all definitely enjoyed working on it. Check it out!\
-    </br></br><a onclick=\"redirect('https://kodykhuu.github.io/covid-sim')\" class='link'>Website</a>\
-    </br><a onclick=\"redirect('https://github.com/brianv212/covid-sim-1')\" class='link'>Github Repository</a>\
-    </br><a onclick=\"redirect('https://devpost.com/software/sus-simulator')\" class='link'>Dev Post</a>"],
-    []
+    </br></br><a href=\"https://kodykhuu.github.io/covid-sim\" target=\"_blank\">Website</a>\
+    </br><a href=\"https://github.com/brianv212/covid-sim-1\" target=\"_blank\">Github Repository</a>\
+    </br><a href=\"https://devpost.com/software/sus-simulator\" target=\"_blank\">DevPost</a>",
+
+    "</br></br>For me personally, I'm interested in the idea of machine learning. It's definitely\
+    a wonder how one can make a machine learn to accurately predict outcomes based purely on\
+    past events, but delving deeper into the topic made me feel like this was a whole new\
+    realm of coding that would be quite difficult to wrap my head around.\
+    </br></br>Nonetheless, I did my fair share of learning and produced a learner that would predict\
+    close to 75% in this kaggle competition. Definitely not the best but a great feat for myself.",
+
+    "</br></br>The game in question was no other than checkers, and my partner and I had quite\
+    a big hurdle to jump over before having a competent AI. In our early stages of development,\
+    we hadn't quite grasped the idea of MCTS, so we would play against other AI's with no clear\
+    indication of a straight-thinking AI - randomly taking moves wasn't enough to clearly gauge\
+    a functioning AI.",
+
+    "",
+
+    "</br></br>Storing data is one thing, but making it efficient was another thing. The class\
+    helped me utilize techniques that are common practice in data management. Starting with E-R\
+    techniques, I learned how to read and create understandable relationships among elements that\
+    would apear in a database. From this foundation we continued to create actual relational tables,\
+    with the addition of understanding syntax to gather information and create views from them.\
+    </br></br>It was definitely most interesting to me simply because I was curious as to how\
+    sites would manage large databases and continue to add more and more information, all at little\
+    to no cost of the runtime to gather it. This class was definitely eye-opening, and a class that\
+    would prepare me for future classes and jobs.",
+
+    "</br></br>Placeholder",
+
+    "</br></br>Placeholder"
+]
 ]
 
 var pictures = [["./pics/algracing1.png","./pics/algracing2.png","./pics/algracing3.png"],
                 ["./pics/esports1.png","./pics/esports2.png"],
                 ["./pics/qfas_1.png", "./pics/qfas_2.png", "./pics/qfas_3.png"],
                 ["./pics/covidsim1.png","./pics/covidsim2.png","./pics/covidsim3.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"],
-                ["./pics/qfas_2.png"]]
-
-// var picture_index = 0;
-// var picture_cycle = 0;
+                ["./pics/machine-learning.jpg"],
+                ["./pics/ai.jpg"],
+                ["./pics/information-retrieval.png"],
+                ["./pics/data-management.jpg"],
+                ["./pics/hci.jpg"],
+                ["./pics/computer-vision.jpg"]]
 
 var topBar = document.querySelector(".pp-topbar");
 var currentProject = document.querySelector(".pp-display");
 var directory = document.querySelector(".pp-directory");
 var redirect = document.querySelector(".changeUrl");
 
-// var readMore = document.querySelector("#read_more_button");
-// var readMoreSection = document.querySelector(".read-more");
-// var readMoreTitle = document.querySelector(".read-more-title")
-// var readMoreExit = document.querySelector(".read-more-exit");
-// var readMoreText = document.querySelector(".read-more-description");
-// var readMoreVisuals = document.querySelector(".read-more-visuals");
-// var readMoreOverlay = document.querySelector(".overlay");
-
 var index_category = 0;
-// var index_category_item = 0;
 
 topBar.addEventListener("click", changeCategory, false);
 function changeCategory(e) {
@@ -174,11 +185,6 @@ function changeCategory(e) {
 
         let item = 0;
         for (let i = 0; i < Math.ceil(titles[index_category].length / 3); i++){
-            // Make a new row
-            // var new_row = document.createElement("DIV");
-            // new_row.className = "pp-display-row";
-
-            // for the next three items: add
             var tracker = 2;
             while (tracker !== -1){
                 console.log(tracker);
@@ -208,7 +214,6 @@ function changeCategory(e) {
         var clicked = 0;
         currentProject.querySelector(".pp-display-title").innerHTML = titles[index_category][parseInt(clicked)];
         currentProject.querySelector(".pp-display-description").innerHTML = text[index_category][parseInt(clicked)] + text_full[index_category][parseInt(clicked)];
-        // index_category_item = parseInt(clicked);
 
         currentProject.className += " show"
     }
@@ -223,18 +228,17 @@ function navigateProject(e){
         console.log(clicked)
         currentProject.querySelector(".pp-display-title").innerHTML = titles[index_category][parseInt(clicked)];
         currentProject.querySelector(".pp-display-description").innerHTML = text[index_category][parseInt(clicked)] + text_full[index_category][parseInt(clicked)];
-        // index_category_item = parseInt(clicked);
-
-        // var element = currentProject.querySelector(".pp-display-pictures");
-        // if (index_category == 0){
-        //     element.querySelector("#image").className = "image";
-        //     element.querySelector("#image").src = pictures[clicked][0];
-        // }
-        // else {
-        //     element.querySelector("#image").className = "image_empty";
-        // }
     }
 }
+
+
+// var readMore = document.querySelector("#read_more_button");
+// var readMoreSection = document.querySelector(".read-more");
+// var readMoreTitle = document.querySelector(".read-more-title")
+// var readMoreExit = document.querySelector(".read-more-exit");
+// var readMoreText = document.querySelector(".read-more-description");
+// var readMoreVisuals = document.querySelector(".read-more-visuals");
+// var readMoreOverlay = document.querySelector(".overlay");
 
 // readMore.addEventListener("click", openReadMore, false)
 // function openReadMore(e){
